ab559bad6e0df9e749c730973aa1581a8c59041c
Williams25 <william.souza.timao@hotmail.com>
2021-02-24 16:27:55 -0300

    Separando server

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,3 @@
-import 'reflect-metadata'
-import './database'
-import dotenv from 'dotenv'
-dotenv.config()
-
-import express from 'express'
-import morgan from 'morgan'
-import routes from './routes'
-import { error } from './middlewares/index'
-
-const app = express()
-
-app.use(express.json())
-app.use(morgan('dev'))
-
-app.use(routes)
-error(app)
+import { app } from './app'
 
 app.listen(process.env.PORT || 3333, () => console.log(`http://localhost:${process.env.PORT || 3333}`))
