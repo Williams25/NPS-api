d3db27f3180ece0fd9a66f625443f61c749beb29
Williams25 <william.souza.timao@hotmail.com>
2021-02-24 16:28:34 -0300

    Criando rota de survey

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,9 @@ import { Router } from 'express'
 const routes = Router()
 
 import UserRoutes from './UserRoutes'
+import SurveyRoutes from './SurveyRoutes'
 
 routes.use('/users', UserRoutes)
+routes.use('/surveys', SurveyRoutes)
 
 export default routes
