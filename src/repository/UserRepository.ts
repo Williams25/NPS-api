10782678ebe4af2b2be4ab316a6aab143971a521
Williams25 <william.souza.timao@hotmail.com>
2021-02-24 16:32:16 -0300

    Fazendo interação com a database

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,4 +1,36 @@
 import { User } from '../models/User'
-import { getRepository } from 'typeorm'
+import { EntityRepository, getRepository, Repository, Like } from 'typeorm'
 
-export const repository = () => getRepository(User)
+interface createInterface {
+  name: string,
+  email: string
+}
+
+interface updateInterface extends createInterface {
+  id: number
+}
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+
+  constructor() {
+    super()
+  }
+
+  static repository() {
+    return getRepository(User)
+  }
+
+  static async create({ name, email }: createInterface) {
+    const user = UserRepository.repository().create({ name, email })
+    return await UserRepository.repository().save(user)
+  }
+
+  static async findOne(campo: string, value: string) {
+    return await UserRepository.repository().findOne({ [campo]: Like(`%${[value]}%`) })
+  }
+
+  static async findAll() {
+    return await UserRepository.repository().find()
+  }
+}
