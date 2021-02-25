248da11c1a6ce50099b20b63f55d31180f304ff4
Williams25 <william.souza.timao@hotmail.com>
2021-02-25 16:44:56 -0300

    Criando a rota para envio de email

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,10 @@ const routes = Router()
 
 import UserRoutes from './UserRoutes'
 import SurveyRoutes from './SurveyRoutes'
+import SendMailRoutes from './SendMailRoutes'
 
 routes.use('/users', UserRoutes)
 routes.use('/surveys', SurveyRoutes)
+routes.use('/sendMail', SendMailRoutes)
 
 export default routes
