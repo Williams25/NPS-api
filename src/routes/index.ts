d1f8b05d4df3b5607f419ce138eb82ef632802c5
Williams25 <william.souza.timao@hotmail.com>
2021-02-26 16:39:51 -0300

    Adicionando novas rotas para answers e nps

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,13 @@ const routes = Router()
 import UserRoutes from './UserRoutes'
 import SurveyRoutes from './SurveyRoutes'
 import SendMailRoutes from './SendMailRoutes'
+import AnswersRoutes from './AnswersRoutes'
+import NpsRoutes from './NpsRoutes'
 
 routes.use('/users', UserRoutes)
 routes.use('/surveys', SurveyRoutes)
 routes.use('/sendMail', SendMailRoutes)
+routes.use('/answers', AnswersRoutes)
+routes.use('/nps', NpsRoutes)
 
 export default routes
