82b6a82f9d9d92825e0122e9e1f47e171ca1052b
Williams25 <william.souza.timao@hotmail.com>
2021-02-26 16:40:13 -0300

    Refatorando sendMail

--- src/services/SendMailServices.ts
+++ src/services/SendMailServices.ts
@@ -36,18 +36,15 @@ class SendMailServices {
       from: "NPS <william007.gabriel@gmail.com>"
     }, (err, info) => {
       if (err) {
-        console.log('Error occurred. ' + err.message);
+        console.log('Error occurred. ' + err.message)
         return process.exit(1);
       }
 
-      console.log('Message sent: %s', info.messageId);
+      console.log('Message sent: %s', info.messageId)
       // Preview only available when sending through an Ethereal account
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
     })
   }
 }
-
-
-
 
 export default new SendMailServices()
